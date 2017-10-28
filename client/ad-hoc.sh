6c667dea2174b07dea2a52eb4094507208bbb7a0
Torrintino <lennart.weiu00df@gmail.com>
2017-10-28 14:40:52 +0200

    Added ad-hoc script

--- client/ad-hoc.sh
+++ client/ad-hoc.sh
@@ -1,6 +1,11 @@
 # join an ad-hoc wifi network
 
-killall wpa_supplicant
-iw wlan0 set type ibss
-iw wlan0 ibss join coding_tag 2432
+read -p "WLAN device name: " WLAN
+read -p "Client ID: " CLIENT_ID
 
+killall wpa_supplicant
+iw $WLAN set type ibss
+ifconfig $WLAN up
+iw $WLAN ibss join coding_tag 2432
+
+ip addr add 192.168.1.$CLIENT_ID/24 broadcast 192.168.1.255 dev $WLAN
