cd0a1dae44abdff37f905a49fc50cf7a9627daa4
Manuel Radatz <manuel.radatz@online.de>
2017-12-08 14:26:15 +0100

    added classes for inter-process communication via unix sockets

---
+++ api/networking.cpp
@@ -0,0 +1,267 @@
+// networking.cpp
+
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#include <limits>
+#include <memory>
+#include <stdexcept>
+#include <utility>
+
+#include "networking.hpp"
+
+
+static void bindLocalUnixSocket(CSocketWrapper& _socket, std::string const& _file_name)
+{
+    struct sockaddr_un name;
+    name.sun_family = AF_LOCAL;
+    
+    if (_file_name.length() >= sizeof(name.sun_path)) {
+        throw std::logic_error("Cannot bind local Unix socket: file name too long");
+    }
+    
+    memcpy(name.sun_path, _file_name.c_str(), _file_name.length());
+    name.sun_path[_file_name.length()] = '\0';
+    
+    int iresult = unlink(_file_name.c_str());
+    
+    if (iresult != 0 && errno != ENOENT) { // ignore non-existing files
+        perror("Error: Cannot unlink file for local Unix socket");
+        throw std::runtime_error("Error: Cannot unlink file " + _file_name
+                                 + " for local Unix socket.");
+    }
+    
+    iresult = bind(_socket.getFD(), (struct sockaddr*)&name,
+                   offsetof(struct sockaddr_un, sun_path) + _file_name.length() + 1);
+    
+    if (iresult != 0) {
+        perror("Error: Cannot bind local Unix socket");
+        throw std::runtime_error("Error: Cannot bind local Unix socket.");
+    }
+}
+
+
+static void sendToSocket(CSocketWrapper& _socket, MessageHeader::TypeType _type,
+                         std::string const& _data)
+{
+    if (_data.length() > std::numeric_limits<MessageHeader::TypeType>::max()) {
+        throw std::logic_error("Data too long.");
+    }
+    
+    MessageHeader header = {
+            (uint32_t)_data.length(), // size
+            _type                     // type
+    };
+    
+    static_assert(sizeof(header) == 8);
+    
+    ssize_t sent = send(_socket.getFD(), &header, sizeof(header), 0);
+    
+    if (sent < 0) {
+        perror("Error: Cannot send message header");
+        throw std::runtime_error("Error: Cannot send message header.");
+    }
+    
+    sent = send(_socket.getFD(), _data.c_str(), _data.length(), 0);
+    
+    if (sent < 0) {
+        perror("Error: Cannot send message");
+        throw std::runtime_error("Error: Cannot send message.");
+    }
+}
+
+
+static void receiveFromSocket(CSocketWrapper& _socket,
+                              bool& _header_written, MessageHeader& _header,
+                              std::function<void(MessageHeader::TypeType, std::string)> _func)
+{
+    ssize_t received;
+    
+    if (!_header_written) {
+        received = recv(_socket.getFD(), &_header, sizeof(_header), 0);
+        
+        if (received < 0) {
+            if (errno == EWOULDBLOCK) // no data is available
+                return;
+            perror("Error: Cannot receive message");
+            throw std::runtime_error("Error: Cannot receive message.");
+        }
+        
+        if (received != sizeof(_header)) {
+            throw std::runtime_error("Error: Did not receive the entire header.");
+        }
+        
+        _header_written = true;
+    }
+    
+    std::unique_ptr<char> data = std::make_unique<char>((size_t)_header.size + 1);
+    
+    received = recv(_socket.getFD(), data.get(), _header.size, 0);
+    
+    if (received < 0) {
+        if (errno == EWOULDBLOCK) // no data is available
+            return;
+        perror("Error: Cannot receive message");
+        throw std::runtime_error("Error: Cannot receive message.");
+    }
+    
+    if (received != _header.size) {
+        throw std::runtime_error("Error: Did not receive the entire message.");
+    }
+    
+    _func(_header.type, std::string(data.get(), (uint32_t)received));
+    
+    _header_written = false;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// CSocketWrapper //////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CSocketWrapper::CSocketWrapper(int _namespace, int _style, int _protocol)
+        : m_sockfd(socket(_namespace, _style, _protocol))
+{
+    m_sockfd = ::socket(_namespace, _style, _protocol);
+    
+    if (m_sockfd == -1) {
+        perror("Error: Cannot create socket");
+        throw std::runtime_error("Error: Cannot create socket.");
+    }
+}
+
+
+CSocketWrapper::CSocketWrapper(CSocketWrapper&& _other)
+        : m_sockfd(std::exchange(_other.m_sockfd, -1))
+{
+    // intentionally empty
+}
+
+
+CSocketWrapper::~CSocketWrapper()
+{
+    if (m_sockfd == -1)
+        return;
+    
+    int iresult = ::close(m_sockfd);
+    
+    if (iresult != 0) {
+        perror("Error: Cannot close connection socket");
+    }
+}
+
+
+CSocketWrapper CSocketWrapper::accept(struct sockaddr* _addr, socklen_t* _length_ptr)
+{
+    int new_sockfd = ::accept(m_sockfd, _addr, _length_ptr);
+    
+    if (new_sockfd == -1) {
+        perror("Error: Cannot accept connection to socket");
+        throw std::runtime_error("Error: Cannot accept connection to socket.");
+    }
+    
+    return CSocketWrapper(new_sockfd);
+}
+
+
+void CSocketWrapper::shutdown(int _how) noexcept
+{
+    int iresult = ::shutdown(m_sockfd, _how);
+    
+    if (iresult != 0) {
+        perror("Error: Cannot shutdown socket");
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// FileDeleter //////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FileDeleter::FileDeleter(std::string const& _file_name)
+        : m_file_name(_file_name)
+{
+    // intentionally empty
+}
+
+
+FileDeleter::~FileDeleter()
+{
+    unlink(m_file_name.c_str()); // ignore errors
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// UnixReceiver ////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+UnixReceiver::UnixReceiver(std::string const& _file_name)
+        : m_socket_file_deleter(_file_name),
+          m_socket(AF_LOCAL, SOCK_DGRAM, 0),
+          m_buffered_header_available(false)
+{
+    bindLocalUnixSocket(m_socket, _file_name);
+    
+    // make socket non-blocking:
+    fcntl(m_socket.getFD(), F_SETFL, O_NONBLOCK); // ignore errors
+}
+
+
+UnixReceiver::~UnixReceiver() = default;
+
+
+void UnixReceiver::receive(std::function<void(MessageHeader::TypeType, std::string)> _func)
+{
+    receiveFromSocket(m_socket, m_buffered_header_available, m_buffered_header, _func);
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// UnixSender //////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+UnixSender::UnixSender(std::string const& _file_name, std::string const& _receiver_file_name)
+        : m_socket_file_deleter(_file_name), m_socket(AF_LOCAL, SOCK_DGRAM, 0)
+{
+    bindLocalUnixSocket(m_socket, _file_name);
+    
+    // connect to server:
+    struct sockaddr_un name;
+    name.sun_family = AF_LOCAL;
+    
+    if (_receiver_file_name.length() >= sizeof(name.sun_path)) {
+        throw std::logic_error("Cannot bind local Unix socket: file name too long.");
+    }
+    
+    memcpy(name.sun_path, _receiver_file_name.c_str(), _receiver_file_name.length());
+    name.sun_path[_receiver_file_name.length()] = '\0';
+    
+    int iresult = connect(m_socket.getFD(), (struct sockaddr*)&name,
+                          offsetof(struct sockaddr_un, sun_path) + _receiver_file_name.length()
+                              + 1);
+    
+    if (iresult != 0) {
+        perror("Error: Cannot connect to server");
+        throw std::runtime_error("Error: Cannot connect to server.");
+    }
+}
+
+
+UnixSender::~UnixSender() = default;
+
+    
+void UnixSender::send(MessageHeader::TypeType _type, std::string const& _data)
+{
+    sendToSocket(m_socket, _type, _data);
+}
---
+++ api/networking.hpp
@@ -0,0 +1,100 @@
+// networking.hpp
+
+#ifndef CODINGTAG_HARDWARE_API_NETWORKING_HPP_
+#define CODINGTAG_HARDWARE_API_NETWORKING_HPP_ 1
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+#include <sys/socket.h>
+
+
+class CSocketWrapper final // to make RAII work (preventig memory leaks)
+{
+public:
+    CSocketWrapper(int _namespace, int _style, int _protocol);
+    CSocketWrapper(CSocketWrapper const& _other) = delete;
+    CSocketWrapper(CSocketWrapper&& _other);
+    CSocketWrapper& operator=(CSocketWrapper const& _other) & = delete;
+    CSocketWrapper& operator=(CSocketWrapper&& _other) & = delete;
+    ~CSocketWrapper();
+    
+    int getFD() { return m_sockfd; }
+    CSocketWrapper accept(struct sockaddr* _addr, socklen_t* _length_ptr);
+            // not needed anymore
+    void shutdown(int _how) noexcept;
+            // not needed anymore
+    
+private:
+    CSocketWrapper(int _sockfd) : m_sockfd(_sockfd) { }
+    int m_sockfd;
+};
+
+
+// deletes file _file_name if it gets destructed.
+class FileDeleter final
+// to make RAII work (preventig open files in case of an exception)
+{
+public:
+    FileDeleter(std::string const& _file_name);
+    FileDeleter(FileDeleter const& _other) = delete;
+    FileDeleter(FileDeleter&& _other);
+    FileDeleter& operator=(FileDeleter const& _other) & = delete;
+    FileDeleter& operator=(FileDeleter&& _other) & = delete;
+    ~FileDeleter();
+    
+private:
+    std::string const m_file_name;
+};
+
+
+struct MessageHeader
+{
+    typedef uint8_t TypeType;
+        // TODO: maybe uint32_t? I'd prefer a sizeof(MessageHeader) of a power of 2.
+    
+    uint32_t size;
+    TypeType type; 
+};
+
+
+class UnixReceiver final
+{
+public:
+    UnixReceiver(std::string const& _file_name);
+    UnixReceiver(UnixReceiver const& _other) = delete;
+    UnixReceiver(UnixReceiver&& _other) = delete;
+    UnixReceiver& operator=(UnixReceiver const& _other) & = delete;
+    UnixReceiver& operator=(UnixReceiver&& _other) & = delete;
+    ~UnixReceiver();
+    
+    void receive(std::function<void(MessageHeader::TypeType, std::string)> _func);
+    
+private:
+    FileDeleter const m_socket_file_deleter;
+    CSocketWrapper m_socket;
+    MessageHeader m_buffered_header;
+    bool m_buffered_header_available;
+};
+
+
+class UnixSender final
+{
+public:
+    UnixSender(std::string const& _file_name, std::string const& _receiver_file_name);
+    UnixSender(UnixSender const& _other) = delete;
+    UnixSender(UnixSender&& _other) = delete;
+    UnixSender& operator=(UnixSender const& _other) & = delete;
+    UnixSender& operator=(UnixSender&& _other) & = delete;
+    ~UnixSender();
+    
+    void send(MessageHeader::TypeType _type, std::string const& _data);
+    
+private:
+    FileDeleter const m_socket_file_deleter;
+    CSocketWrapper m_socket;
+};
+
+
+#endif // CODINGTAG_HARDWARE_API_NETWORKING_HPP_
