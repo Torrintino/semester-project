170c5bf65e94f2ecf4be426458781dfb73f2fba4
Rafael Robert Hadamik <rafael.robert.hadamik@informatik.hu-berlin.de>
2017-12-12 22:37:41 +0100

    .c für den Reciever

---
+++ driver/IrRecieveD.c
@@ -0,0 +1,178 @@
+#include "IrRecieveD.h"
+#include <lirc/lirc_client.h>
+#include <fcntl.h>
+#include <string.h>
+
+//0,wenn erfolgreich, initialisiert den lircd-Socket
+int initReciever(){
+	int socketId;
+	if((socketId=lirc_init("codingTagR",1))==-1){
+		printf("Fehler beim Connecten zu Lirc\n");
+		return -1;
+	}
+	if(fcntl(socketId,F_SETFL,O_NONBLOCK)==-1){
+	printf("Fehler beim Blockieren aufheben\n");
+	return -1;
+	}
+	return socketId;
+}
+//Liest den Input vom Socket aus
+int readInput(char *code){
+	int ret;
+	ret = -1;
+
+	if(lirc_nextcode(&code)==0){
+		if(code==NULL){
+			return 0;
+		}
+		else{
+			switch(code[24]){
+				case'1':
+					ret = 1;
+					break;
+				case'2':
+					ret = 2;
+					break;
+				case'3':
+					ret = 3;
+					break;
+				case'4':
+					ret = 4;
+					break;
+				case'5':
+					ret = 0;
+					break;
+				case'6':
+					ret = 0;
+					break;
+				case'7':
+					ret = 0;
+					break;
+				case'8':
+					ret = 0;
+					break;
+				case'9':
+					ret = 0;
+					break;
+				case'A':
+					ret = 0;
+					break;
+				case'B':
+					ret = 0;
+					break;
+				case'C':
+					ret = 0;
+					break;
+				case'D':
+					ret = 0;
+					break;
+				case'E':
+					ret = 0;
+					break;
+				case'F':
+					ret = 0;
+					break;
+				default:
+					break;
+			}
+		}
+		free(code);
+	}
+	return ret;
+}
+//Leert falls notwendig(Bsp. nach Timeout wegen Treffer)
+int flushReciever(char*code){
+	lirc_nextcode(&code);
+	while(code !=NULL){
+		lirc_nextcode(&code);
+	}
+return 0;
+}
+
+//Disconnect vom Socket, sobald nicht mehr nötig
+int deinitReciever(){
+	lirc_deinit();
+}
+//Code senden, blockt, aber bei 16bit kein Problem
+int sendCode(int sendPlayer){
+
+	int fd;
+	char player[5];
+	char remote[2];
+	strcpy(remote,"12");
+	strcpy(player,"KEY_");
+	if(sendPlayer>15){
+		printf("Spieleranzahl überstiegen.");
+		return -1;
+	}
+	switch(sendPlayer){
+		case 1: strcat(player,"1");
+			break;
+		case 2: strcat(player,"2");
+			break;
+		case 3: strcat(player,"3");
+			break;
+		case 4: strcat(player,"4");
+			break;
+		case 5: strcat(player,"5");
+			break;
+		case 6: strcat(player,"6");
+			break;
+		case 7: strcat(player,"7");
+			break;
+		case 8: strcat(player,"8");
+			break;
+		case 9: strcat(player,"9");
+			break;
+		case 10: strcat(player,"A");
+			break;
+		case 11: strcat(player,"B");
+			break;
+		case 12: strcat(player,"C");
+			break;
+		case 13: strcat(player,"D");
+			break;
+		case 14: strcat(player,"E");
+			break;
+		case 15: strcat(player,"F");
+			break;
+	}
+	fd= lirc_get_local_socket(NULL,0);
+	if(fd<1){
+	printf("Keine Verbindung zum Socket möglich");
+	return -1;
+	}
+	fflush(stdout);
+	if(lirc_send_one(fd,remote,player )==-1){
+		printf("Konnte keinen Code senden");
+		return -1;
+	}
+	return 0;
+}
+/*Testecke
+int main(){
+	int k;
+	char *code;
+	code = (char*)malloc(sizeof(char));
+	initReciever();
+	sendCode(1);
+	while(1==1){
+		for(int i=0;i<10000;i++){
+			for(int j=0;j<10000;j++){
+			k=i+j;
+			}
+		}
+		flushReciever(code);
+		for(int i=0;i<10000;i++){
+			for(int j=0;j<10000;j++){
+			k=i+j;
+			}
+		}
+		k=readInput(code);
+		printf("%u",k);
+		fflush(stdout);
+	}
+	deinitReciever();
+	return 0;
+}
+*/
