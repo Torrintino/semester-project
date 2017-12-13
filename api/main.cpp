1da0d61099295b6243aed68ecdf9d49217dffb18
Manuel Radatz <radatzmq@informatik.hu-berlin.de>
2017-12-13 19:55:58 +0100

    added signal handler, prepared for incoming messages from Services, corrected use of the IR driver, Sending IR data still TODO

--- api/main.cpp
+++ api/main.cpp
@@ -1,47 +1,94 @@
 // api/main.cpp
 
+#include <atomic>
 #include <cstdlib>
+#include <cstdio>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
 
-#include "interface-to-services.hpp"
+#include <signal.h>
 
-extern "C" {
-    #include "../driver/IrRecieveD.h"
-}
+#include "interface-to-services.hpp"
+#include "../driver/IrRecieveD.h"
 
 
 using namespace codingtag;
 using namespace codingtag::hardware;
 
+
+static bool termination_requested;
+
+
+extern "C" {
+    static void signal_handler(int _signum, siginfo_t* _siginfo, void* _context)
+    {
+        fprintf(stderr, "Received SIGTERM.\n");
+        termination_requested = true;
+    }
+}
+
+
 int main()
 {
+    termination_requested = false;
+    
+    
+    // ignore SIGPIPEs:
+    signal(SIGPIPE, SIG_IGN);
+    
+    
+    // handle SIGTERMs correctly:
+    struct sigaction action;
+    
+    memset(&action, 0, sizeof(action));
+    action.sa_sigaction = signal_handler;
+    action.sa_flags = SA_SIGINFO;
+    
+    if (sigaction(SIGTERM, &action, NULL) != 0) {
+        perror("Error: Signal handler for SIGTERM could not be set");
+        return EXIT_FAILURE;
+    }
+    
+    
     // init IR driver:
     {
-        int iresult = initReciever();
+        int iresult = initReceiver();
         
         if (iresult != 0) {
             std::cerr << "Error: IR Driver initialization failed." << std::endl;
             return EXIT_FAILURE;
         }
-    
     }
     
     
     try {
         InterfaceToServices interface;
         
-        while (true) { // TODO: When should it terminate?
+        while (!termination_requested) {
+            try {
+                interface.checkForReceivedMessages();
+            }
+            catch (MessageParsingError& e) {
+                std::cerr << "Error: Catched MessageParsingError: " << e.what() << std::endl;
+            }
+            catch (MessageOfUnknownTypeError& e) {
+                std::cerr << "Error: Catched MessageOfUnknownTypeError: " << e.what() << std::endl;
+            }
+            catch (ReceivedUnexpectedMessageException& e) {
+                std::cerr << "Error: Catched ReceivedUnexpectedMessageException: "
+                << e.what() << std::endl;
+            }
+            
             
             // TODO: send own ID about every second OR alternatively wait for input from Services
             
-            char buffer[64]; // TODO: Is that way of using readInput correct?
-            uint32_t received_id = readInput(buffer); // TODO: Does it block?
+            char buffer[64];
+            int received_id = readInput(buffer);
             
-            if (true) { // TODO: only if there is a received ID
+            if (received_id > 0) {
                 try {
-                    interface.receiveIR(received_id);
+                    interface.receiveIR((uint32_t)received_id);
                 }
                 catch (ConnectFailedNoSuchFileException& e) {
                     std::cerr << "Error: Catched ConnectFailedNoSuchFileException: "
@@ -70,10 +117,10 @@ int main()
         goto failure;
     }
     
-    deinitReciever();
+    deinitReceiver();
     return EXIT_SUCCESS;
     
 failure:
-    deinitReciever();
+    deinitReceiver();
     return EXIT_FAILURE;
 }
