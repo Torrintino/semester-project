673e92e49eb8b76eb525389ea5217f3f4911931b
Rafael Robert Hadamik <rafael.robert.hadamik@informatik.hu-berlin.de>
2017-12-13 18:59:45 +0100

    Update IrRecieveD.h

--- driver/IrRecieveD.h
+++ driver/IrRecieveD.h
@@ -4,19 +4,20 @@
 #include <string.h>
 
 //ACHTUNG: char* code ist nur ein Array pointer, den das Hauptprogramm aufsetzen sollte,
-	   aus Gründen der Effizienz und meiner eigenen Unfähigkeit
+//	   aus Gründen der Effizienz und meiner eigenen Unfähigkeit
 
 
 //0,wenn erfolgreich, initialisiert den lircd-Socket
-int initReciever();
+int initReceiver();
 	
 //Liest den Input vom Socket aus, sollte im Hauptprogramm 
 // in der Loop laufen (Verbrauch von Max 20 Ticks beim Empfangen von Daten)
+//bei keinem Input, -1 bei Fehler, sonst Spielernummer
 int readInput(char *code);
 
 //Leert falls notwendig(Bsp. nach Timeout wegen Treffer)
-int flushReciever(char*code);
+int flushReceiver(char*code);
 //Disconnect vom Socket, sobald nicht mehr nötig
-int deinitReciever();
+int deinitReceiver();
 //Code senden, blockt, aber bei 16bit kein Problem
 int sendCode(int sendPlayer);
