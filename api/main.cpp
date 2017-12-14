f3f4faaf3c95c08273740de60e48f3bff99c67c6
Manuel Radatz <manuel.radatz@online.de>
2017-12-14 19:32:27 +0100

    sends own client ID now

--- api/main.cpp
+++ api/main.cpp
@@ -9,6 +9,7 @@
 #include <stdexcept>
 
 #include <signal.h>
+#include <unistd.h>
 
 #include "interface-to-services.hpp"
 #include "../driver/IrRecieveD.h"
@@ -54,6 +55,59 @@ int main()
     }
     
     
+    // get own client id:
+    uint32_t own_client_id;
+    {
+        char hostname[256];
+        
+        int iresult = gethostname(hostname, 256);
+        
+        if (iresult != 0) {
+            perror("Error: Could not get hostname which is required for client identification");
+            return EXIT_FAILURE;
+        }
+        
+        // check whether the hostname starts with "client-":
+        if (memcmp(hostname, "client-", 7) != 0) {
+            std::cerr << "Error: Hostname \"" << hostname
+                      << "\" has invalid pattern. The pattern \"client-N\" was expected."
+                      << std::endl;
+            return EXIT_FAILURE;
+        }
+        
+        own_client_id = 0;
+        
+        // parse the client id:
+        for (size_t i = 7; i < 11; ++i) {
+            if (hostname[i] == '\0') {
+                break;
+            }
+            else if (i >= 10) {
+                std::cerr << "Error: Hostname \"" << hostname
+                          << "\" has invalid pattern. The pattern \"client-N\" was expected where "
+                             "\"N\" is a positive integer with maximum 3 digits." << std::endl;
+                return EXIT_FAILURE;
+            }
+            else if (hostname[i] >= '0' && hostname[i] <= '9') {
+                own_client_id *= 10;
+                own_client_id += static_cast<uint32_t>(hostname[i] - '0');
+            }
+            else {
+                std::cerr << "Error: Hostname \"" << hostname
+                          << "\" has invalid pattern. The pattern \"client-N\" was expected where "
+                             "\"N\" is a positive integer with maximum 3 digits." << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+        
+        if (own_client_id == 0) {
+            std::cerr << "Error: Hostname \"" << hostname
+                      << "\" not allowed. Client ID must be bigger than 0." << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+    
+    
     // init IR driver:
     {
         int iresult = initReceiver();
@@ -93,8 +147,7 @@ int main()
                 
                 if (current_timepoint - last_timepoint > 500ms) {
                     last_timepoint = current_timepoint;
-                    
-                    sendCode(2); // TODO : What is my own client ID?
+                    sendCode(static_cast<int>(own_client_id));
                 }
             }
             
